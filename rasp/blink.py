84b1a5142cbf569268d12bc21f8cbbd3ac7f9fef
yellowBunny <yellow.bunny51@gmail.com>
2019-03-03 20:15:24 +0100

    add class PinControler

--- rasp/blink.py
+++ rasp/blink.py
@@ -3,36 +3,25 @@ import RPi.GPIO as GPIO
 import json
 print('imprted {}'.format(GPIO.__name__))
 
+class PinControler():
+    STATUS = 'nothing'
 
-def set_pin(pin, signal):
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, signal)
-    if signal:
-    	signal = 'ON'
-    else:
-    	signal = 'OFF'
-    dane2 = dict(signal=signal) 
-    data = json.JSONEncoder() # konwersja danych z pythona na JSON
-    dane = data.encode(dane2)
-    return dane
-    
+    def set_pins(self, pin, signal):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, signal)
+        if signal:
+            signal = 'ON'
+            self.STATUS = signal
+        else:
+            signal = 'OFF'
+            self.STATUS = signal
+        dane2 = dict(signal=signal)
+        data = json.JSONEncoder() # konwersja danych z pythona na JSON
+        dane = data.encode(dane2)
+        return dane
 
-def t_func():
-    return 'udalo sie'
 
-def home(request):
-    return HttpResponse('HOME PAGE')
-
-def turnOn(request):
-	# set_pin(21,1)
-    print('gpio on')
-    return HttpResponse(set_pin(21,1))
-
-def turnOff(request):
-    print('gpio off')
-    # set_pin(21,0)
-    return HttpResponse(set_pin(21,0))
 
 
 
--- raspi/views.py
+++ raspi/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
 import os
 print(os.getcwd())
-from rasp import blink as b
+from rasp.blink import PinControler
 from django.http import HttpResponse
 
 # Create your views here.
+pinControler = PinControler()
 
 def home(request):
-    varible = b.t_func
-    return render(request, 'home.html', {'on': varible})
+    status = pinControler.STATUS
+    return render(request, 'home.html', {'on': status})
 
 def blink(request):
-    varible = b.t_func
-    return render(request, 'blink.html', {'on': varible})
+    status = pinControler.STATUS
+    return render(request, 'blink.html', {'status': status})
 
 def process(request):
-    print('tu process')    
-    return HttpResponse(b.set_pin(21,1))
+    print('tu process')
+    return HttpResponse(pinControler.set_pins(21,1))
 
 def process2(request):
-    print('tu process2')    
-    return HttpResponse(b.set_pin(21,0))
+    print('tu process2')
+    return HttpResponse(pinControler.set_pins(21,0))
 
 
